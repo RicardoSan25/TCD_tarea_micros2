abc8d9283e6cd846ae102c168d92313764d3e635
juanbervj19 <137564440+juanbervj19@users.noreply.github.com>
2023-06-23 16:45:47 -0500

    Update teclas.h

--- lib/teclas/teclas.h
+++ lib/teclas/teclas.h
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 
+// Definición de estados con un tipo de dato enumerativo
 typedef enum {
     BU1_BU2,
     BU1_BD2,
@@ -8,6 +9,7 @@ typedef enum {
     rebotes
 }var_estado; 
 
+//Estructura con parámetros de pulsante 1 y 2 y ademas el estado de la MEF
 typedef struct{
     int16_t push1;
     int16_t push2; 
@@ -15,7 +17,7 @@ typedef struct{
     var_estado estado1_push;
 } struct_actualizar;  
 
-bool* inicializar_MEF(int16_t push1, int16_t push2,int16_t time2);
-var_estado actualizar_MEF(struct_actualizar vstr_actualizar); 
-bool buttonPressed(int16_t tecla); 
-bool buttonReleased(int16_t tecla); 
+bool* inicializar_MEF(int16_t push1, int16_t push2,int16_t time2);  //Iniciar MEF
+var_estado actualizar_MEF(struct_actualizar vstr_actualizar);       // Actualizar MEF
+bool buttonPressed(int16_t tecla);                                 // Acciones correspondientes al flanco DESCENDENTE, se presiono pulsante
+bool buttonReleased(int16_t tecla);                                // Acciones correspondientes al flanco ASCENDENTE, se libero pulsante
